4800ab8a5656bdcee7b5fa905882fb0d1f4de2af
Wellington Leite <wellingtonleitee.s@gmail.com>
2023-04-23 17:55:55 -0300

    fix: solve issue when access the bike details page without a bike object

--- src/pages/BikeDetails/BikeDetails.container.tsx
+++ src/pages/BikeDetails/BikeDetails.container.tsx
@@ -1,21 +1,28 @@
-import { useEffect } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import BikeDetails from './BikeDetails.component'
 import { useRentBike } from './BikeDetails.utils'
-import { Paths } from 'routes/paths'
+import Bike from 'models/Bike'
+import { useEffect, useState } from 'react'
+
+type StateReceived = {
+  bike: Bike
+}
 
 const BikeDetailsContainer = () => {
   const { state } = useLocation()
-  const navigate = useNavigate()
   const { booked, isLoading, onSubmit } = useRentBike()
+  const [currentBikeData, setCurrentBikeData] = useState<Bike>()
 
   useEffect(() => {
-    if (!state?.bike) {
-      navigate(Paths.HOME)
+    if (state) {
+      const { bike } = state as StateReceived
+      setCurrentBikeData(bike)
     }
   }, [])
 
-  return <BikeDetails bike={state.bike} booked={booked} isLoading={isLoading} onSubmit={onSubmit} />
+  return (
+    <BikeDetails bike={currentBikeData} booked={booked} isLoading={isLoading} onSubmit={onSubmit} />
+  )
 }
 
 export default BikeDetailsContainer
